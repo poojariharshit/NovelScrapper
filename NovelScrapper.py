389985012ca9006137ff5e94ca35d57a096d517d
poojariharshit <35663027+poojariharshit@users.noreply.github.com>
2020-08-25 19:27:57 +0530

    Update NovelScrapper.py

--- NovelScrapper.py
+++ NovelScrapper.py
@@ -2,16 +2,17 @@ import scrapy
 
 class QuotesSpider(scrapy.Spider):
     name = "novels"
-    start_urls = ['https://www.wuxiaworld.co/I-Shall-Seal-the-Heavens/']
+    start_urls = ['https://www.wuxiaworld.co/example/']
 
     def parse(self, response):
         for novel in response.css('div.chapter-wrapper a.chapter-item'):
-            
+            #This scrapes the main page of the novel for the links of individual chapters
             yield response.follow(novel, callback=self.parse_page)
-            #yield response.follow(a, callback=self.parse)
+            #The spider follows the link to the page 
 
 
     def parse_page(self, response):
+        #This scrapes the text of the particular page
         yield {
             'text': response.css('.chapter-entity::text').getall(),
             }
